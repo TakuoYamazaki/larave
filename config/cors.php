c97559fb1bfa031193ce965d4c7c86727d877217
Yamazaki Takuo <2155085@stu.hus.ac.jp>
2023-11-06 16:28:52 +0900

    cors設定

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://tko-react-app.s3-website-ap-northeast-1.amazonaws.com'],
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
